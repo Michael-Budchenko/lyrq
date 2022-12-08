b2d51595a7b06056c0bc3a7ae5352c7f180e39c0
Michael-Budchenko <92309983+Michael-Budchenko@users.noreply.github.com>
2022-12-09 00:42:47 +0200

    first commit

---
+++ src/js/_functions.js
@@ -0,0 +1,8 @@
+
+import { mobileCheck } from "./functions/mobile-check";
+console.log(mobileCheck())
+
+
+import { burger } from './functions/burger';
+
+
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import './_functions';
+
+
+$(function () {
+
+  $('.slider').slick({
+    slidesToShow: 2.07,
+    slidesToScroll: 1,
+  });
+
+});
